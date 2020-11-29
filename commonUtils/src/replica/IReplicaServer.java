ecb21fd0e91e4a116bb94456d1dea9e09ff010a4
mcalixte <mike.calixte@yahoo.ca>
2020-11-28 21:09:11 -0500

    Created Replica Response Bean

--- commonUtils/src/replica/IReplicaServer.java
+++ commonUtils/src/replica/IReplicaServer.java
@@ -1,2 +1,17 @@
-package replica;public interface IReplicaServer {
+package replica;
+
+public interface IReplicaServer {
+    //Manager Invokable methods
+    public String addItem(String managerID, String itemID, String itemName, int quantity, double price);
+    public String removeItem(String managerID, String itemID, int quantity) ;
+    public String listItemAvailability (String managerID);
+
+    //Client Invokable methods
+    public String purchaseItem (String customerID, String itemID, String dateOfPurchase);
+    public String findItem (String customerID, String itemName);
+    public String returnItem (String customerID, String itemID, String dateOfReturn) ;
+    public String exchange(String customerID, String newItemID, String oldItemID, String dateOfReturn);
+
+    public void requestUpdateOfCustomerBudgetLog(String customerID, double price);
+    public boolean waitList(String customerID, String itemID, String dateOfPurchase);
 }
--- commonUtils/src/replica/ReplicaResponse.java
+++ commonUtils/src/replica/ReplicaResponse.java
@@ -1,4 +1,46 @@
 package replica;
 
-public class ReplicaResponse {
+import networkEntities.RegisteredReplica;
+
+import java.io.Serializable;
+
+public class ReplicaResponse implements Serializable {
+        private RegisteredReplica replicaID;
+        private boolean success;
+        private String response;
+
+        public ReplicaResponse(RegisteredReplica replicaID, boolean success, String response) {
+            this.replicaID = replicaID;
+            this.success = success;
+            this.response = response;
+        }
+
+        public RegisteredReplica getReplicaID() {
+            return replicaID;
+        }
+
+        public void setReplicaID(RegisteredReplica replicaID) {
+            this.replicaID = replicaID;
+        }
+
+        public boolean getSuccessResult() {
+            return success;
+        }
+
+        public void setSuccessResult(boolean success) {
+            this.success = success;
+        }
+
+        public String getResponse() {
+            return response;
+        }
+
+        public void setResponse(String response) {
+            this.response = response;
+        }
+
+        @Override
+        public String toString() {
+            return "ReplicaResponse [replicaID=" + replicaID + ", success=" + success + ", response=" + response + "]";
+        }
 }
